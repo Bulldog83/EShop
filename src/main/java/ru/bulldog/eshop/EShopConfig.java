4205f1c411a12aff3fc3d0239d79097e053c041d
Aleksey <bulldog.seregin@gmail.com>
2021-06-27 12:15:45 +0300

    Reload

--- src/main/java/ru/bulldog/eshop/EShopConfig.java
+++
@@ -1,8 +0,0 @@
-package ru.bulldog.eshop;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.bulldog.eshop")
-public class EShopConfig {}
--- src/main/java/ru/bulldog/eshop/repository/EntityRepo.java
+++
@@ -1,9 +0,0 @@
-package ru.bulldog.eshop.repository;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface EntityRepo<T> {
-	Optional<T> getOne(long id);
-	List<T> getAll();
-}
