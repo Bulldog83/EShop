14b36c2cf01e5d469e73ccc9db5a42aa09db2afe
Aleksey <bulldog.seregin@gmail.com>
2021-08-20 22:59:59 +0300

    Spring Framework. E-Shop. Homework 3 - statistics

---
+++ src/main/java/ru/bulldog/eshop/web/StatController.java
@@ -0,0 +1,26 @@
+package ru.bulldog.eshop.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.bulldog.eshop.statistics.StatisticsService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/statistics")
+public class StatController {
+
+	private final StatisticsService statisticsService;
+
+	@Autowired
+	public StatController(StatisticsService statisticsService) {
+		this.statisticsService = statisticsService;
+	}
+
+	@GetMapping
+	public Map<String, Long> getTimeStatistics() {
+		return statisticsService.getTimeStatistics();
+	}
+}
